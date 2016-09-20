3b557069155c69cc198e001b97f5acd1dcc1a4f2
Andreas Oehlke <andreas.oehlke@comdirect.de>
2016-09-20 18:09:29 +0200

    init

---
+++ app/src/main/java/de/comdirect/collabothon2016/collabothon2016/MainActivity.java
@@ -0,0 +1,164 @@
+package de.comdirect.collabothon2016.collabothon2016;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener,
+        GroupOverviewFragment.OnFragmentInteractionListener,
+        VotingFragment.OnFragmentInteractionListener {
+
+    @BindView(R.id.nav_drawer_content)
+    FrameLayout frameLayout;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) frameLayout.getLayoutParams();
+        // FIXME Hoehe ist gehackt fuer das HTC One M9
+        lp.setMargins(0, 160, 0, 0);
+//        lp.setMargins(0, UiUtils.getStatusBarHeight(this), 0, 0);
+
+        onShowGroupOverview();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_item_group_overview:
+                onShowGroupOverview();
+                break;
+            case R.id.nav_item_voting:
+                onShowVoting();
+                break;
+            case R.id.nav_item_send:
+            case R.id.nav_item_share:
+                Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                throw new IllegalStateException("Unknown nav item selected.");
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void onShowGroupOverview() {
+        GroupOverviewFragment frag = GroupOverviewFragment.newInstance(null, null);
+        String tag = frag.getClass().getSimpleName();
+
+        FragmentManager fm = getSupportFragmentManager();
+        boolean hasBackStackEntries = fm.getBackStackEntryCount() > 0;
+
+        FragmentTransaction ft = fm.beginTransaction();
+        if (hasBackStackEntries) {
+            ft.addToBackStack(null);
+        }
+        ft.replace(R.id.nav_drawer_content, frag, tag);
+        ft.commit();
+    }
+
+    private void onShowVoting() {
+        VotingFragment frag = VotingFragment.newInstance(null, null);
+        String tag = frag.getClass().getSimpleName();
+
+        FragmentManager fm = getSupportFragmentManager();
+        boolean hasBackStackEntries = fm.getBackStackEntryCount() > 0;
+
+        FragmentTransaction ft = fm.beginTransaction();
+        if (hasBackStackEntries) {
+            ft.addToBackStack(null);
+        }
+        ft.replace(R.id.nav_drawer_content, frag, tag);
+        ft.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+    }
+
+}
---
+++ app/src/main/java/de/comdirect/collabothon2016/collabothon2016/UiUtils.java
@@ -0,0 +1,14 @@
+package de.comdirect.collabothon2016.collabothon2016;
+
+public class UiUtils {
+
+//    public static int getStatusBarHeight(Context context) {
+//        int result = 0;
+//        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+//        if (resourceId > 0) {
+//            result = context.getResources().getDimensionPixelSize(resourceId);
+//        }
+//        return result;
+//    }
+
+}
